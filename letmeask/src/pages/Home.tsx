ca2072ff59cf6ba825a09091e35ae434cb92ff41
evenilsonlp <evenilsonlp@gmail.com>
2021-06-23 22:25:00 -0300

    AULA 02: Autenticação com firebase

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -4,13 +4,23 @@ import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
+
+
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
+import firebase from 'firebase';
+import { auth } from '../services/firebase';
 
 export function Home () {
   const history = useHistory();
 
-  function navigateToNewRoom() {
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then( result => {
+      console.log(result);
+    })
+
     history.push('/rooms/new');
   }
 
@@ -24,7 +34,7 @@ export function Home () {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button onClick={navigateToNewRoom} className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
